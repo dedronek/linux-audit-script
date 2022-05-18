b0522320eadcc7ad135069e261e93b341b21bc27
dawidkedron <dawidkedron@debian>
2022-05-18 21:20:36 +0200

    Add tests for audit ssh

--- tests/audit_ssh.sh
+++ tests/audit_ssh.sh
@@ -46,3 +46,23 @@ echo -n "correct permissions for public host key files ... "
         echo $failure
         failed=$( expr $failed + 1 )
       fi
+
+#ROOT NEEDED
+
+if [[ "$root" -eq 1 ]]; then
+  cmd_check "SSH access is limited ... " 'sshd -T -C user=root -C host="$(hostname)" -C addr="$(grep $(hostname) /etc/hosts | awk '\''{print $1}'\'')" | grep -Ei '\''^\s*(allow|deny)(users|groups)\s+\S+'\''' "output"
+  cmd_check "SSH usage is logging ... " 'grep -is '\''loglevel'\'' /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf | grep -Evi '\''(VERBOSE|INFO)'\'''
+
+  q1=`sshd -T -C user=root -C host=\"$(hostname)\" -C addr=\"$(grep $(hostname) /etc/hosts | awk '{print $1}')\" | grep -E 'maxauthtries [0-4]'`
+  q2=`grep -Eis '^\s*maxauthtries\s+([5-9]|[1-9][0-9]+)' /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf`
+
+  echo -n "MaxAuthTries set to 4 or less ... "
+
+  if [[ "$q1" != "" && "$q2" == "" ]]; then
+        echo $success
+        score=$( expr $score + 1 )
+      else
+        echo $failure
+        failed=$( expr $failed + 1 )
+      fi
+  fi
