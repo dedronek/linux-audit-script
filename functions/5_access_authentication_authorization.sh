9a3ab4b45a2290650d4b8a4f3cd29f9a06aa4789
dawidkedron <dawidkedron@debian>
2022-05-22 00:17:00 +0200

    Add test for PAM and user accounts and envs

--- functions/5_access_authentication_authorization.sh
+++ functions/5_access_authentication_authorization.sh
@@ -1,6 +1,8 @@
 cd tests
-. audit_cron.sh
-. audit_sudo.sh
-. audit_ssh.sh
+#. audit_cron.sh
+#. audit_sudo.sh
+#. audit_ssh.sh
+#. audit_pam.sh
+. audit_user_acc_and_env.sh
 
 cd ..
---
+++ tests/audit_pam.sh
@@ -0,0 +1,32 @@
+echo "Auditing PAM..."
+
+echo -n "password length >=14, password complexity (minclass = 4) ... "
+
+    q1=`grep '^\s*minlen\s*' /etc/security/pwquality.conf | grep -Eo '[0-9]{1,4}'`
+    q2=`bash -c "grep '^\s*minclass\s*' /etc/security/pwquality.conf"`
+    q3=`grep -E '^\s*[duol]credit\s*' /etc/security/pwquality.conf | grep -o "\-1" | wc -l`
+
+    if [[ $q1 -ge 14 ]] && [[ $q2 -ge 4  ]] || [[ "$q3" == "4" ]]; then
+        echo $success
+        score=$( expr $score + 1 )
+      else
+        echo $failure
+        failed=$( expr $failed + 1 )
+      fi
+
+echo -n "failed password attempts lockout is configured ... "
+
+    q1=`grep "pam_tally2" /etc/pam.d/common-auth`
+    q2=`grep -E "pam_(tally2|deny)\.so" /etc/pam.d/common-account | wc -l`
+
+    if [[ $q1 != "" && $q2 == "2" ]]; then
+        echo $success
+        score=$( expr $score + 1 )
+      else
+        echo $failure
+        failed=$( expr $failed + 1 )
+      fi
+
+cmd_check "reusing password is limited (5 or more old password remembered) ... " 'grep -E '\''^\s*password\s+required\s+pam_pwhistory\.so\s+([^#]+\s+)?remember=([5-9]|[1-9][0-9]+)\b'\'' /etc/pam.d/common-password' "output"
+
+cmd_check "password hashing algorithm sha512 ... " 'grep -E '\''^\s*password\s+(\[success=1\s+default=ignore\]|required)\s+pam_unix\.so\s+([ ^#]+\s+)?sha512\b'\'' /etc/pam.d/common-password' "output"
