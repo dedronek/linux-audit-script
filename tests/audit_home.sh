0423a57579e48653c896a6d3ab68d162b7006335
dedronek <dk.dedron@gmail.com>
2022-05-28 18:31:35 +0200

    Fix audit for home dir

--- tests/audit_home.sh
+++ tests/audit_home.sh
@@ -3,6 +3,6 @@ echo "Auditing /home..."
 cmd_check "/home exists on separate partition ... " "findmnt /home" "output"
 
 if [ $? -eq 1 ]; then
-  cmd_check "nodev option set for /home ... " "findmnt -n /home | grep -v nodev" "empty"
+  cmd_check "nodev option set for /home ... " "findmnt -n /home | grep -v nodev"
   fi
 
